d2de75eec3cffcba197e5a89f7d37dd2aa2b0e88
yaojie <limyaojie93@gmail.com>
2018-02-01 09:43:19 +0800

    update routes to go to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "main#home"
+  authenticated :user do
+    root 'main#home', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root "devise/sessions#new"
+  end
 end
