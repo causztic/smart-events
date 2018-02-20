e4e5faa4c41368d54a22bb9e1fd1759090f6100b
yaojie <limyaojie93@gmail.com>
2018-02-20 21:32:03 +0800

    install packages properly

---
+++ app/javascript/components/calendar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+window.React = React;
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+
+export const Calendar = props => (
+  <div>
+    <BigCalendar
+      events={myEventsList}
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
+  </div>
+);
