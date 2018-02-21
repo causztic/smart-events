9fa6dc48c703f68b10bc39540ed95543555112ac
yaojie <limyaojie93@gmail.com>
2018-02-21 12:18:54 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180220040012) do
     t.string "locate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
