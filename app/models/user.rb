c3511336fe3356ae4f2ec99324f5dfb6bd924141
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2018-02-28 03:04:37 +0800

    changed back to role. no progress done. giving up on life.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
   
   ROLES = [:coordinator, :student, :instructor]
 
-  if User.has_Role? :student, :instructor
+  if User.has_role? :student, :instructor
 	has_and_belong_to_many :subjects
    end
 
   def send_message
 
-	if User.has_Role? :instructor, :coordinator
+	if User.has_role? :instructor, :coordinator
 
 	end
   
@@ -20,11 +20,11 @@ class User < ApplicationRecord
 
   def review_message
 
-	if User.has_Role? :instructor
+	if User.has_role? :instructor
 		#review own message history
 	end
   
-	if User.has_Role? :coordinator
+	if User.has_role? :coordinator
 		#review all message history
 	end
   end
