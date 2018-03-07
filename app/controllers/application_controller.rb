392b337f471cc7c1708627dd7258a00f7db787b3
yaojie <limyaojie93@gmail.com>
2018-03-07 18:44:22 +0800

    use user specific partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :user_partial
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_partial
+    @user_partial ||= current_user.type.underscore if current_user
   end
 
   def authenticate_user!
