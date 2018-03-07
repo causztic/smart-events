7797f96e9331400f2b085baebaa5ca344ae81272
yaojie <limyaojie93@gmail.com>
2018-03-07 10:50:13 +0800

    refactor validates

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,14 +2,6 @@ class Subject < ApplicationRecord
     self.primary_key = :code
     has_many :lessons, dependent: :destroy
 
-    validates :code, presence: true
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :hours_per_week, presence: true
-    validates :facility_hours, presence: true
-    validates :minimum_hours_per_lesson, presence: true
-    validates :term_available, presence: true
-
+    validates :code, :name, :description, :hours_per_week, :facility_hours, :minimum_hours_per_lesson, :term_available, presence: true
     enum pillar: [ :Freshmore, :EPD, :ESD, :ISTD, :ASD, :PillarYear ]
-    # enum pillar: [ 0: freshmore, 1: epd, 2: esd, 3: istd, 4: asd, 5: unspecified ]
 end
