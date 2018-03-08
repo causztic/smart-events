eb03cd2c4f1fa36a87e9d5ae6ec0b3e23f034065
Victor <veeklee92@gmail.com>
2018-03-09 05:44:03 +0800

    changed event_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-    {start_time > end_time}
+    {end_time>=start_time}.should be_true
+    validates :end_time, presence: true, time: {after_or_equal_to: :start_time}
+    validates :end_date, presence: true, date: {after_or_equal_to: :start_date}
+    validates :name, presence: true
+    validates :speaker_name, presence: true
+    validates :duration, numericality => { :greater_than_or_equal_to => 0, :less_than_or_Equal_to => 600}
+    validates :date, date.valid_date?
+    validates :pillar, 
 end
