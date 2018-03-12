2d350adab6183ffc826d4e358e6b89d0b7c01a89
yaojie <limyaojie93@gmail.com>
2018-03-12 22:50:55 +0800

    change facility hours to jsonb instead of json, fix session if user is not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :user_partial
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      reset_session
+    end
   end
 
   def user_partial
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312033438) do
+ActiveRecord::Schema.define(version: 20180312143805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,16 +55,6 @@ ActiveRecord::Schema.define(version: 20180312033438) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
-  end
-
   create_table "sessions", force: :cascade do |t|
     t.time "start_time"
     t.time "end_time"
@@ -87,7 +77,7 @@ ActiveRecord::Schema.define(version: 20180312033438) do
     t.string "name"
     t.string "description"
     t.float "hours_per_week"
-    t.json "facility_hours"
+    t.jsonb "facility_hours"
     t.float "minimum_hours_per_lesson"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -116,14 +106,6 @@ ActiveRecord::Schema.define(version: 20180312033438) do
     t.string "avatar"
     t.string "slug"
     t.integer "pillar", default: 0
-  end
-
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "role_id"
-    t.index ["role_id"], name: "index_users_roles_on_role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
-    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
 end
