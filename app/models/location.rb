7f2372090c3a7fa7ca41ab4aba28d235e7fc9028
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2018-03-14 02:32:09 +0800

    rename location type data field in factories and included type and capacity validation in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 # Location of where any lesson-session is being held.
 class Location < ApplicationRecord
   has_many :sessions
-  validates :name, :roomname, presence: true
+  validates :name, :roomname, :capacity, :type, presence: true
 end
