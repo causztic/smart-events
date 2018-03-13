a49319212b40aaa2962ba1e2404cb05ad88a66d5
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2018-03-14 03:41:51 +0800

    added subject validatino fields

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,6 +2,9 @@ require "rails_helper"
 
 RSpec.describe Lesson, type: :model do
   it { should belong_to(:subject) }
+  it { should belong_to(:location)}
+
+  it{ should validate_presence_of(:subject, :duration, :start_time, :end_time, :location)}
 
   describe "it should have multiple sessions to accomodate for student size" do
     it { should have_many(:sessions) }
