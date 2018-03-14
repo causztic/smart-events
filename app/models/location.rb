3a8fd14faf0411a3f1cd55ad7303103bd5481872
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2018-03-14 12:57:49 +0800

    changed type to location_type

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 # Location of where any lesson-session is being held.
 class Location < ApplicationRecord
   has_and_belongs_to_many :sessions
-  validates :name, :roomname, :capacity, :type, presence: true
+  validates :name, :roomname, :capacity, :location_type, presence: true
 end
--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :location do
     name "Default Location"
     roomname "Room Name"
-    type "Classroom"
+    location_type "Classroom"
     capacity 40
     locate "Location"
   end
