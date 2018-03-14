2a04f8b07c92333a324629b4be2ea1c64c88bd0a
yaojie <limyaojie93@gmail.com>
2018-03-14 10:59:16 +0800

    add custom event validation example

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+
+  validate :start_time_must_be_earlier
+
+  def start_time_must_be_earlier
+    errors.add(:start_time, "start time must be earlier than end time!") if start_time > end_time
+  end
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-    {end_time>=start_time}.should be_true
-    validates :end_time, presence: true, time: {after_or_equal_to: :start_time}
-    validates :end_date, presence: true, date: {after_or_equal_to: :start_date}
-    validates :name, presence: true
-    validates :speaker_name, presence: true
-    validates :duration, numericality => { :greater_than_or_equal_to => 0, :less_than_or_Equal_to => 600}
-    validates :date, date.valid_date?
-    validates :pillar, 
+    # {end_time>=start_time}.should be_true
+    # validates :end_time, presence: true, time: {after_or_equal_to: :start_time}
+    # validates :end_date, presence: true, date: {after_or_equal_to: :start_date}
+    # validates :name, presence: true
+    # validates :speaker_name, presence: true
+    # validates :duration, numericality => { :greater_than_or_equal_to => 0, :less_than_or_Equal_to => 600}
+    # validates :date, date.valid_date?
+    # validates :pillar,
+    describe "time validations" do
+        it "should to allow start time to be later than end_time" do
+            event = build(:event)
+            event.start_time = Time.now
+            event.end_time = Time.now - 1.hour
+            expect(event).to_not be_valid
+        end
+    end
 end
