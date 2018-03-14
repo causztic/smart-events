aedef60111a171239055eae4c3073b9c22115341
yaojie <limyaojie93@gmail.com>
2018-03-15 00:04:59 +0800

    add demo sessions from backend to calendar

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,5 +3,14 @@ class SchedulesController < ApplicationController
   def show
     #@events = user.sessions
     @events = []
+    test_subject_name = current_user.subjects.first.name;
+    30.times do |t|
+      @events << {
+        id: t,
+        title: test_subject_name,
+        start: Time.now.beginning_of_day + 10.hours + (7 * t).days,
+        end:  Time.now.beginning_of_day + 12.hours + (7 * t).days
+      }
+    end
   end
 end
--- app/javascript/components/calendar.jsx
+++ app/javascript/components/calendar.jsx
@@ -7,10 +7,20 @@ export class Calendar extends Component {
     super(props)
     BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
     this.state = {
-      allViews: ['month', 'work_week'],
-      events: this.props.events
+      allViews: Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]),
+      events: []
     }
   }
+
+  componentDidMount(){
+    this.setState({ events: this.props.events.map((event) => {
+        event.start = new Date(event.start);
+        event.end = new Date(event.end);
+        return event;
+      })
+    });
+  }
+
   render() {
     return  (<div style={{height: "400px"}}>
       <BigCalendar
