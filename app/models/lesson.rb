3716da3373d5698a795b80fb5f3529ce68f44b07
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2018-03-14 11:37:29 +0800

    moved start and end time validation from lesson to session

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,7 +4,7 @@ class Lesson < ApplicationRecord
   belongs_to :location
   has_many :sessions
 
-  validates :subject, :duration, :start_time, :end_time, :location, presence: true
+  validates :subject, :duration, :location, presence: true
 
   enum location_type: ::LOCATION_TYPES
 end
