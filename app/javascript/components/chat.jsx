486fee43523e2be97be8300a9748b4b02f7f54fb
yaojie <limyaojie93@gmail.com>
2018-03-21 00:30:14 +0800

    prep for multiple chat channels per user

--- app/javascript/components/chat.jsx
+++ app/javascript/components/chat.jsx
@@ -1,14 +1,12 @@
 import React, { PureComponent } from 'react';
 import {ActionCableProvider, ActionCable} from 'react-actioncable-provider';
 
-export class Chat extends PureComponent {
+export class ChatChannel extends PureComponent {
   constructor(props){
     super(props);
-    this.toggleChatWindow = this.toggleChatWindow.bind(this);
     this.onReceived = this.onReceived.bind(this);
     this.state = {
-      chatWindow: false,
-      messages: [],
+      messages: []
     }
   }
 
@@ -24,28 +22,46 @@ export class Chat extends PureComponent {
   sendMessage = () => {
     const message = this.refs.newMessage.value
     // Call perform or send
-    this.refs.roomChannel.send({message: message, room: this.props.chat_room_id})
+    this.refs.roomChannel.send({message: message, room: this.props.room})
   }
 
+  render() {
+    const { messages } = this.state;
+    return (
+    <div>
+      <ActionCable ref='roomChannel' channel={{channel: 'MessagesChannel', room: this.props.room}} onReceived={this.onReceived} />
+      <ul>
+        {messages.map((m) =>
+          <li key={m.message.id}>{m.message.content}</li>
+        )}
+      </ul>
+      <input ref='newMessage' type='text' />
+      <button onClick={this.sendMessage}>Send</button>
+    </div>
+    )
+  }
+}
+
+export class Chat extends PureComponent {
+  constructor(props){
+    super(props);
+    this.toggleChatWindow = this.toggleChatWindow.bind(this);
+    this.state = {
+      chatWindow: false
+    }
+  }
   toggleChatWindow() {
     this.setState({ chatWindow: !this.state.chatWindow });
   }
 
   render() {
-    const { chatWindow, messages } = this.state;
+    const { chatWindow } = this.state;
     return (
       <ActionCableProvider>
-        <ActionCable ref='roomChannel' channel={{channel: 'MessagesChannel', room: this.props.chat_room_id}} onReceived={this.onReceived} />
         <div className='chat-area'>
           { chatWindow &&
             <div className='chat-window'>
-            <ul>
-            {messages.map((m) =>
-              <li key={m.message.id}>{m.message.content}</li>
-            )}
-            </ul>
-            <input ref='newMessage' type='text' />
-            <button onClick={this.sendMessage}>Send</button>
+              {this.props.chat_room_ids.map((id) =>  <ChatChannel key={id} room={id}/>)}
             </div>
           }
           <a className='chat-icon' onClick={this.toggleChatWindow}>💬</a>
