56de60c55d0ef2ca30656941741308e38d3e87cc
yaojie <limyaojie93@gmail.com>
2018-03-27 01:10:07 +0800

    convert hours to day-specific objects

--- app/javascript/components/timeslot-picker.jsx
+++ app/javascript/components/timeslot-picker.jsx
@@ -1,34 +1,51 @@
-import React, { PureComponent } from 'react';
-import AvailableTimes from 'react-available-times';
+import React, { PureComponent } from "react";
+import AvailableTimes from "react-available-times";
+import axios from "axios";
 
 export class TimeSlotPicker extends PureComponent {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
+    this.days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
   }
+
+  getTime(rawValue) {
+    const hour = (rawValue / 60) % 24;
+    const day = this.days[~~(rawValue / 60 / 24)];
+    return { day: day, hour: `${~~hour}:${60 * (hour % 1)}` };
+  }
+
+  updateSelectedTimes(selections) {
+    times = selections.map(({ start, end }) => {
+      const { day, startHour: hour } = this.getTime(start);
+      const { endHour: hour } = this.getTime(end);
+      let obj = new Object();
+      obj[day] = { start: startHour, end: endHour };
+      console.log(obj);
+      return obj;
+    });
+  }
+
   render() {
     return (
       <AvailableTimes
         weekStartsOn="monday"
         calendars={[
           {
-            id: 'work',
-            title: 'Work',
-            foregroundColor: '#ff00ff',
-            backgroundColor: '#f0f0f0',
-            selected: true,
+            id: "work",
+            title: "Work",
+            foregroundColor: "#ff00ff",
+            backgroundColor: "#f0f0f0",
+            selected: true
           }
         ]}
-        onChange={(selections) => {
-          selections.forEach(({ start, end }) => {
-            console.log('Start:', start, 'End:', end);
-          })
-        }}
+        onChange={selections => this.updateSelectedTimes(selections)}
         recurring={true}
-        availableDays={['monday', 'tuesday', 'wednesday', 'thursday', 'friday']}
+        availableDays={this.days}
         availableHourRange={{
           start: 8.5,
           end: 18
         }}
-      />)
+      />
+    );
   }
 }
