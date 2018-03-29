3db1bc64bf370144a385d8619882341cc1aa481f
yaojie <limyaojie93@gmail.com>
2018-03-29 16:30:01 +0800

    add some mock statistics

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,4 +5,17 @@ class Session < ApplicationRecord
   belongs_to :subject
   belongs_to :location
   enum day: Date::DAYNAMES
+
+  before_update :check_student_conflicts, :check_location_conflicts, :check_instructor_conflicts
+
+  private
+  def check_student_conflicts
+  end
+
+  def check_location_conflicts
+  end
+
+  def check_instructor_conflicts
+  end
+
 end
