725f758e0aa7a173746383376ab13a562f8cac1c
yaojie <limyaojie93@gmail.com>
2018-03-30 13:09:58 +0800

    update boolean logic for conflict checking, alert if conflict

--- app/models/session.rb
+++ app/models/session.rb
@@ -13,8 +13,8 @@ class Session < ApplicationRecord
     sql = "SELECT s.location_id, s.instructor_id, array_agg(u.student_id) students FROM sessions s
       INNER JOIN sessions_users u ON s.id = u.session_id
       WHERE s.id IN
-      (SELECT id FROM sessions WHERE start_time BETWEEN '#{start_time}'::timestamp AND '#{end_time - 1.second}'::timestamp
-        OR end_time BETWEEN '#{start_time + 1.second}'::timestamp AND '#{end_time}'::timestamp
+      (SELECT id FROM sessions WHERE (start_time BETWEEN '#{start_time}'::timestamp AND '#{end_time - 1.second}'::timestamp
+        OR end_time BETWEEN '#{start_time + 1.second}'::timestamp AND '#{end_time}'::timestamp)
       AND id != '#{id}')
       GROUP BY(s.id);"
     results = ActiveRecord::Base.connection.execute(sql)
