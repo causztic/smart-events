97e9b694ebcb3f7a0819628072afb0f4d4496012
yaojie <limyaojie93@gmail.com>
2018-04-01 18:45:55 +0800

    fix failing factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,11 +1,9 @@
 FactoryBot.define do
   factory :event do
-    events_model 1.5
     name "Event"
     description "Description"
     speaker_name "Speaker Name"
-    duration 40.0
     date 20180315
-    pillar "Pillar"
+    pillar 0
   end
 end
